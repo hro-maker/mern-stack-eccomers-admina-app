eedee190d5ad4d5f85692c0fecccaa30f9dcc8ca
hro-maker <hrant.muradyan.1998@mail.ru>
2021-02-14 11:57:29 +0400

    chenjed api

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,5 @@
-export const api= 'https://shlyanscart-rest-server.herokuapp.com/api'
+export const api= 'https://eccomers-rest-server.herokuapp.com/api'
+// export const api= 'http://localhost:2000/api'
 export const generatepublicurl=(filename)=>{
     return `http://localhost:2000/public/${filename}`
 }
